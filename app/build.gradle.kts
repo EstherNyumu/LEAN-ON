a8de9e0c5a9c08faace3ee3af443fbef30f451cb
EstherNyumu <emutheu1998@gmail.com>
2024-02-23 10:43:36 +0300

    gradle changes'
    ;
    ext
    exit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
 //    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
